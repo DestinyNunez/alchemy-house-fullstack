f16e8605044c00dd02100cce65faa4d7edf42cb0
DestinyNunez <88361309+DestinyNunez@users.noreply.github.com>
2022-05-30 22:16:48 -0400

    first commit

---
+++ config/database.js
@@ -0,0 +1,9 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://cruddemo:TGW4Bnddm5l4nHjN@cluster0.t24xs.mongodb.net/?retryWrites=true&w=majority',
+    'dbName': 'complete-login-one'
+};
+
+
+/* database name before question mark */
